6aef8e83ab9845b318ec1a13dbc7e86e5a89ef07
Romeoteni188 <romeoteni093@gmail.com>
2026-02-05 15:47:56 -0600

    feat(Footer): Date functio added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 export default function Footer() {
+    const year = new Date().getFullYear()
     return (
         <footer className="bg-slate-200 py-8 text-center text-xs text-slate-700 dark:bg-slate-900 dark:text-slate-400">
-            <p>
-                © 2025 10enconta – haciendo realidad tu idea de forma rentable
+            <p className="select-none">
+                © {year} 10enconta – haciendo realidad tu idea de forma rentable
             </p>
             <div className="mt-2 flex justify-center gap-4">
                 <a href="/politicaPrivacidad" className="underline">
