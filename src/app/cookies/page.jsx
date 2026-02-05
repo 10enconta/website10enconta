354b147bcb3f0076421db661476b603e43045a84
Romeoteni188 <romeoteni093@gmail.com>
2026-02-05 15:41:52 -0600

    feat(cookies): create module cookies

---
+++ src/app/cookies/page.jsx
@@ -0,0 +1,179 @@
+const SectionOne = () => {
+    return (
+        <div className="flex justify-center w-full h-auto text-center select-none py-6 ">
+            <div className="max-w-3xl ">
+                <h1 className=" font-bold text-emerald-600 text-3xl">
+                    Política de Cookies
+                </h1>
+                <p className="mt-4 text-left">
+                    Si estás leyendo esto, es porque quiero explicarte con
+                    claridad qué hago con tus datos personales y cómo los
+                    protejo.
+                </p>
+
+                <h3 className="mt-2 font-black text-left">Recuerda:</h3>
+
+                <p className="mt-2 text-left">
+                    Puedes permitir o bloquear las cookies, así como borrar tus
+                    datos de navegación (incluidas las cookies) desde el
+                    navegador que utilizas. Consulta las opciones e
+                    instrucciones que ofrece tu navegador para ello. Ten en
+                    cuenta que, si aceptas las cookies de terceros, deberás
+                    eliminarlas desde las opciones del navegador.
+                </p>
+
+                <h1 className=" font-black mt-6 text-left">
+                    ¿Quién es el responsable del tratamiento de mis cookies?
+                </h1>
+                <ul className="mt-4 text-left list-disc list-inside">
+                    <li>
+                        <strong>Nombre comercial:</strong> 10enconta
+                    </li>
+                    <li>
+                        <strong>Responsable:</strong> 10enconta
+                    </li>
+                    <li>
+                        <strong>País:</strong> Guatemala
+                    </li>
+                    <li>
+                        <strong>Correo electrónico:</strong>
+                        <a
+                            href="mailto:privacidad@10enconta.com"
+                            className="text-emerald-600 underline">
+                            privacidad@10enconta.com
+                        </a>
+                    </li>
+                </ul>
+
+                <p className="mt-4 text-left">
+                    Puedes permitir o bloquear las cookies, así como borrar tus
+                    datos de navegación (incluidas las cookies) desde el
+                    navegador que utilizas. Consulta las opciones e
+                    instrucciones que ofrece tu navegador para ello. Ten en
+                    cuenta que, si aceptas las cookies de terceros, deberás
+                    eliminarlas desde las opciones del navegador.
+                </p>
+            </div>
+        </div>
+    )
+}
+const Sectiontwo = () => {
+    return (
+        <div className="flex justify-center w-full h-auto text-center select-none py-6 ">
+            <div className="max-w-3xl ">
+                <h1 className=" font-black text-left">
+                    ¿Qué es una cookie y qué información almacena?
+                </h1>
+                <p className="mt-2 text-left">
+                    En general hay tres tipos de cookies:
+                    <strong>
+                        de análisis de usuarios, de funcionamiento web y las
+                        publicitarias.
+                    </strong>
+                </p>
+                <p className="mt-2 text-left">
+                    <strong>Las cookies de análisis </strong>
+                    de usuarios rastrean el uso que haces (anónimamente o no) en
+                    una web, lo que visitas, el orden en el que visitas, el
+                    tiempo que estás en una misma página, ubicación geográfica,
+                    incluso edad o sexo dependiendo de la aceptación de
+                    políticas de privacidad que hayas realizado en otras
+                    plataformas, Google, Facebook, Bing, etc.
+                </p>
+                <p className="mt-2 text-left">
+                    <strong>Las cookies de funcionamiento de una web </strong>
+                    sirven para configurar las funciones que tiene dicha web,
+                    por ejemplo, si pulsas un botón de compra, que el producto
+                    se añada al carrito de la compra y puedas seguir navegando
+                    sin perder el producto añadido, incluso si sales del sitio
+                    web y vuelves en un tiempo determinado.
+                </p>
+                <p className="mt-2 text-left">
+                    <strong>Las cookies de publicitarias, </strong>
+                    sirven para ejecutar acciones de marketing, por ejemplo, si
+                    ves un producto de una tienda y sales de ella, que sigas
+                    viendo ese y otros productos de esa tienda en otros sitios
+                    web, buscadores web o cualquier otro sitio donde se permita
+                    la publicidad.
+                </p>
+                <p className="mt-6 text-left">
+                    Las cookies no suelen almacenar información sensible sobre
+                    usted, como tarjetas de crédito o datos bancarios,
+                    fotografías o información personal, etc. Los datos que
+                    guardan son de carácter técnico, estadísticos, preferencias
+                    personales, personalización de contenidos, etc.
+                </p>
+                <p className="mt-6 text-left">
+                    El servidor web no le asocia a usted como persona sino a su
+                    navegador web. De hecho, si usted navega habitualmente con
+                    el navegador Chrome y prueba a navegar por la misma web con
+                    el navegador Firefox, verá que la web no se da cuenta que es
+                    usted la misma persona porque en realidad está asociando la
+                    información al navegador, no a la persona.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+const SectionThree = () => {
+    return (
+        <div className="flex justify-center w-full h-auto text-center select-none py-6 ">
+            <div className="max-w-3xl ">
+                <h2 className="mt-2 font-black text-left">
+                    Cookies aceptadas por la normativa y cookies exceptuadas
+                </h2>
+                <p className="mt-4 text-left">
+                    Según la directiva de la UE, las cookies que requieren el
+                    consentimiento informado por parte del usuario son las
+                    cookies de analítica y las de publicidad, quedando
+                    exceptuadas las de carácter técnico y las necesarias para el
+                    funcionamiento del sitio web o la prestación de servicios
+                    expresamente solicitados por el usuario.
+                </p>
+                <ul className="mt-4 text-left list-disc list-inside">
+                    <li>
+                        <strong>Finalidades:</strong> Los datos recabados a
+                        través de las cookies de esta web serán tratados de
+                        acuerdo con las finalidades indicadas en las
+                        descripciones de cada una de las cookies de la presente
+                        Política de Cookies.
+                    </li>
+                    <li>
+                        <strong>Base de legitimación:</strong> Para la
+                        instalación de las cookies es necesario su
+                        consentimiento, a excepción de las cookies estrictamente
+                        necesarias para el correcto funcionamiento de la web
+                        (cookies técnicas) que están exentas de consentimiento
+                        de acuerdo con lo establecido en la normativa y
+                        expresado en este apartado.
+                    </li>
+                    <li>
+                        <strong>Plazos de conservación:</strong> Los diferentes
+                        servicios o cookies pueden tener plazos diferentes de
+                        conservación de los datos (o caducidad de la propia
+                        cookie). Se encuentran definidos en el propio módulo de
+                        configuración de cookies.
+                    </li>
+                    <li>
+                        <strong>Comunicación de sus datos:</strong>Los datos de
+                        los usuarios podrán ser tratados por el responsable de
+                        esta web y por terceros que instalan cookies (cookies de
+                        terceros), de acuerdo con sus propias políticas de
+                        privacidad.
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default function Cookies() {
+    return (
+        <div className="flex flex-col ">
+            <SectionOne />
+            <Sectiontwo />
+            <SectionThree />
+        </div>
+    )
+}
