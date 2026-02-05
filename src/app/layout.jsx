7c8c20024d8f8f1b0a83a9ac2a8021be11493f20
Romeoteni188 <romeoteni093@gmail.com>
2026-02-05 12:51:39 -0600

    feat(layout): add footer y gogle analitcs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,29 +1,41 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+// import Navbar from "@/components/Navbar"
+import Footer from "@/components/Footer"
+import GoogleAnalytics from "@/components/GoogleAnalytics"
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+})
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+})
 
 export const metadata = {
-  title: "10enconta",
-  description: "Contabilidad Guatemala",
-};
+    title: "10enconta",
+    description: "Contabilidad Guatemala",
+}
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" data-theme="light" suppressHydrationWarning>
+            <body
+                className={`${geistSans.className} ${geistMono.className} antialiased`}>
+                <GoogleAnalytics />
+                <div
+                    style={{
+                        minHeight: "100vh",
+                        display: "flex",
+                        flexDirection: "column",
+                    }}>
+                    {/* <Navbar /> */}
+                    <main style={{ flexGrow: 1 }}>{children}</main>
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    )
 }
