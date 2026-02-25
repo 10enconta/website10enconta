dc9cd0d894ad07f030c44c986ac8869390590032
Romeoteni188 <romeoteni093@gmail.com>
2026-02-25 16:38:28 -0600

    feat(route.js): add api contact

---
+++ src/app/api/contact/route.js
@@ -0,0 +1,34 @@
+import { Resend } from "resend"
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+
+export async function POST(req) {
+    try {
+        const { email } = await req.json()
+
+        if (!email) {
+            return Response.json(
+                { error: "Email es requerido" },
+                { status: 400 },
+            )
+        }
+
+        await resend.emails.send({
+            from: "Landing <onboarding@resend.dev>",
+            to: "10enconta@gmail.com",
+            subject: "Nuevo registro 10enconta oficial",
+            html: `
+                <h2>Nuevo cliente interesado</h2>
+                <p><strong>Email:</strong> ${email}</p>
+            `,
+        })
+
+        return Response.json({ success: true })
+    } catch (error) {
+        console.error(error)
+        return Response.json(
+            { error: "Error enviando correo" },
+            { status: 500 },
+        )
+    }
+}
