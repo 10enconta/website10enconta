65010fc2b70d80231589e22b1343fddaf493129b
Romeoteni188 <romeoteni093@gmail.com>
2026-02-26 08:52:11 -0600

    fix(route.js): change email

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -14,7 +14,7 @@ export async function POST(req) {
         }
 
         await resend.emails.send({
-            from: "Landing <contacto@10enconta.com>",
+            from: "Landing <contacto@data.10enconta.com>",
             to: "romeoteni093@gmail.com",
             subject: "Nuevo registro 10enconta oficial",
             html: `
